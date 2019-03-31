1f54130301718c826012ccc1c86dc39507efe571
ganes891 <ganesan.kandasami@gmail.com>
2019-03-31 19:04:12 +0530

    the new job6 test in container1

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,3 @@
-FROM ganesh891/httpd_new1:latest
+FROM ganesh891/httpd:latest
 COPY /tmp/workspace/devproject/ /usr/local/apache2/htdocs
 EXPOSE 80
--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 docker login -u ganesh891 -p ganesh-1
 docker commit job5 httpd_new1
-docker tag http_new1 ganesh891/http_new1
-docker push ganesh891/http_new1
+docker tag httpd_new1 ganesh891/httpd_new1
+docker push ganesh891/httpd_new1
 docker stop job5
 docker rm job5
-docker build -t httpd_new2 /tmp/workspace/devproject/
-docker run -dit --name job6 -p 80:80 httpd_dev1:devproject
+docker build -t httpddev:devproject /tmp/workspace/devproject/
+docker run -dit --name job6 -p 80:80 httpddev:devproject
