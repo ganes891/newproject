2fc881d1968e3fd80164c0921b14f07c04b8ffba
ganes891 <ganesan.kandasami@gmail.com>
2019-04-01 10:06:57 +0530

    the new job6 test in container2

--- script.sh
+++ script.sh
@@ -1,12 +1,15 @@
-docker login -u ganesh891 -p ganesh-1
-docker commit job5 httpd_new1
-docker tag httpd_new1 ganesh891/httpd_new1
-docker push ganesh891/httpd_new1
-docker stop job5
-docker rm job5
 cd /tmp/workspace/devproject/
 cd ../
 cd -
-ls -ltr
-docker build -t httpddev:devproject /tmp/workspace/devproject/
-docker run -dit --name job6 -p 80:80 httpddev:devproject
+docker login -u ganesh891 -p ganesh-1
+docker commit job12 httpd_new1
+docker tag httpd_new12 ganesh891/http_new12
+docker push ganesh891/httpd_new12
+docker stop job12
+docker rm job12
+docker pull ganesh891/httpd
+docker tag ganesh891/httpd ganesh891/httpd_devproj:1955pm
+docker run -dit --name job6 -p 80:80 -v /tmp/workspace/devproject/:/usr/local/apache2/htdocs ganesh891/httpd_devproj:1955pm
+
+
+
