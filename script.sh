e2584a3174405839c51f1e87dfe634f12c52402d
ganes891 <ganesan.kandasami@gmail.com>
2019-04-02 13:11:09 +0530

    the first Container

--- script.sh
+++ script.sh
@@ -2,13 +2,15 @@ cd /tmp/workspace/devproject/
 cd ../
 cd -
 docker login -u ganesh891 -p ganesh-1
-docker commit job6 httpd_20
-docker tag httpd_6 ganesh891/httpd_6
-docker push ganesh891/httpd_6
-docker stop job6
-docker rm job6
-docker build -t httpd:devproject7 /tmp/workspace/devproject/
-docker tag httpd:devproject7 ganesh891/httpd_07_image
-docker run -dit --name job6 -p 80:80 -v /tmp/workspace/devproject/:/usr/local/apache2/htdocs ganesh891/httpd_07_image
+docker commit job7 httpd_7
+docker tag httpd_7 ganesh891/httpd_7
+docker push ganesh891/httpd_7
+docker stop job7
+docker rm job7
+sleep 10
+docker build -t httpd:devproject8 /tmp/workspace/devproject/
+docker tag httpd:devproject8 ganesh891/httpd_08_image
+docker run -dit --name job8 -p 80:80 ganesh891/httpd_08_image
 sleep 5
-docker cp /tmp/workspace/devproject/. job7:/usr/local/apache2/htdocs
+#docker cp /tmp/workspace/devproject/. job7:/usr/local/apache2/htdocs
+
